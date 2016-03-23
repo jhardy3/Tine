82b670c32651820fb7fa3cb396c39fd036ed1bcf
Jake Hardy <jhardy3@live.com>
2016-03-23 11:38:14 -0600

    Finished the photo controller

---
+++ Tine/LogInViewController.swift
@@ -0,0 +1,40 @@
+//
+//  logInViewController.swift
+//  Tine
+//
+//  Created by Jake Hardy on 3/23/16.
+//  Copyright © 2016 NSDesert. All rights reserved.
+//
+
+import UIKit
+
+class logInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        if HunterController.sharedInstance.currentHunter == nil {
+            self.performSegueWithIdentifier("loggedIn", sender: self)
+        }
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
